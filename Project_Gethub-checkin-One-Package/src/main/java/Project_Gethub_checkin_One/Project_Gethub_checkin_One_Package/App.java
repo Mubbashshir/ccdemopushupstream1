47ac1da3e6ba36217de8d21a58a2fad5a28dd247
Mubbashshir Mohiuddin <Mubbashshir77@gmail.com>
2019-11-27 14:51:37 +0000

    Checkin code

---
+++ Project_Gethub-checkin-One-Package/src/main/java/Project_Gethub_checkin_One/Project_Gethub_checkin_One_Package/App.java
@@ -0,0 +1,13 @@
+package Project_Gethub_checkin_One.Project_Gethub_checkin_One_Package;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
